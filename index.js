2722fa203107e92913733ae6fd86a3600f422950
Tim Patrick Kakucs <tim.patrick@kakucs.de>
2019-10-09 23:35:10 +0200

    Fixed an Issue with Date
    The Date would not format correctly, because the keyword "new" was missing

--- index.js
+++ index.js
@@ -473,7 +473,7 @@ function logIn(userName, password, res) {
                 res.send({state: 'failed', reason: err});
                 return;
               }
-              res.send({state: 'succeeded', data: {token: token, ttl: ttl, createdAt: Date().toISOString()}});
+              res.send({state: 'succeeded', data: {token: token, ttl: ttl, createdAt: new Date()}});
               saveToken(token, userName, function(err) {});
             });
             return;
